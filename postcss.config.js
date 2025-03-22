6468edb35c528b4ad21b26c361ca2c4ada88b8a6
matheusgmdr7 <matheusgmdr7@gmail.com>
2025-03-22 02:55:40 -0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
+    autoprefixer: {
+      // Usar a configuração do Browserslist do projeto
+      overrideBrowserslist: undefined,
+    },
   },
 }
 
