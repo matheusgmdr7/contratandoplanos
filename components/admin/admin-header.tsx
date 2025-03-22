6922c59387ed4f3da8f2181db729ec38404eb447
matheusgmdr7 <matheusgmdr7@gmail.com>
2025-03-22 02:34:07 -0300

    Create admin-header.tsx

---
+++ components/admin/admin-header.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+export default function AdminHeader() {
+  return (
+    <header className="bg-white border-b border-gray-200 h-16">
+      <div className="flex items-center justify-between h-full px-4">
+        <div></div>
+      </div>
+    </header>
+  )
+}
+
