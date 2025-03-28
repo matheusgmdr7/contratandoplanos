8308e892fe15e2c8e7c39589a18665c72d7970fc
matheusgmdr7 <matheusgmdr7@gmail.com>
2025-03-28 16:48:44 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    webpackBuildWorker: true,
+  },
   images: {
-    domains: ["jtzbuxoslaotpnwsphqv.supabase.co"],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: "https://jtzbuxoslaotpnwsphqv.supabase.co",
-    NEXT_PUBLIC_SUPABASE_ANON_KEY:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0emJ1eG9zbGFvdHBud3NwaHF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1MDU5MDEsImV4cCI6MjA1ODA4MTkwMX0.jmI-h8pKW00TN5uNpo3Q16GaZzOpFAnPUVO0yyNq54U",
+    domains: ['images.unsplash.com', 'plus.unsplash.com'],
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
 
