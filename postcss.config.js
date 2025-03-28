adbceeaef2e8c41a272d7b559876d69b04af03a9
matheusgmdr7 <matheusgmdr7@gmail.com>
2025-03-28 03:26:08 -0300

    Delete postcss.config.js

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {
-      // Usar a configuração do Browserslist do projeto
-      overrideBrowserslist: undefined,
-    },
-  },
-}
