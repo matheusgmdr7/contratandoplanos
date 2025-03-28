bf144f7c320a94fa21e2ed06bb3fb5e34ccd45cb
matheusgmdr7 <matheusgmdr7@gmail.com>
2025-03-28 03:22:49 -0300

    Delete app directory

--- app/planos/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-16 h-16 border-4 border-blue-200 border-t-brand-blue rounded-full animate-spin"></div>
-    </div>
-  )
-}
-
