b9785510d90138845e3f4cb6cc459051f1806eba
matheusgmdr7 <matheusgmdr7@gmail.com>
2025-04-07 00:54:27 -0300

    Update page.tsx

--- app/corretor/(dashboard)/tabelas/page.tsx
+++ app/corretor/(dashboard)/tabelas/page.tsx
@@ -1,7 +1,8 @@
-"use client"
+import { CardDescription } from "@/components/ui/card"
+;("use client")
 
 import { useState, useEffect } from "react"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
