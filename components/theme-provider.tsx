83a433b2b9a6627cad8ff62368824b5822f570c4
matheusgmdr7 <matheusgmdr7@gmail.com>
2025-04-07 01:05:54 -0300

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -4,3 +4,4 @@ import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "ne
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
+
